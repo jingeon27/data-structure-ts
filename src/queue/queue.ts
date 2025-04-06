557f91d7f848fe480c56e35d5faf38165d84e9bd
jingeon27 <jingeon27@gmail.com>
2025-04-07 00:33:42 +0900

    queue 생성

---
+++ src/queue/queue.ts
@@ -0,0 +1,23 @@
+export class Queue<T> {
+  constructor(private readonly items: T[] = []) {}
+
+  enqueue(item: T) {
+    this.items.push(item);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  peek() {
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+}
