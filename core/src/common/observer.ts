f9f9aa56b12d71070c9578c80b592b896bf8098d
gun kim <gun@whattime.co.kr>
2025-04-09 00:14:37 +0900

    type 일관성 맞추기

--- core/src/common/observer.ts
+++ core/src/common/observer.ts
@@ -1,15 +1,15 @@
 export class Observable {
-  private listeners = new Set<() => void>();
+  private listeners = new Set<VoidFunction>();
 
   notify() {
     this.listeners.forEach((listener) => listener());
   }
 
-  subscribe(listener: () => void) {
+  subscribe(listener: VoidFunction) {
     this.listeners.add(listener);
   }
 
-  unsubscribe(listener: () => void) {
+  unsubscribe(listener: VoidFunction) {
     this.listeners.delete(listener);
   }
 }
