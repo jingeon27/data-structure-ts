025069466f7ec9fc299d69ff8eed73a45567698f
jingeon27 <jingeon27@gmail.com>
2025-04-13 23:34:52 +0900

    functionkey로 key 제한하기

--- react/src/common/bindMap.ts
+++ react/src/common/bindMap.ts
@@ -1,5 +1,9 @@
+export type FunctionKeys<T> = {
+  [K in keyof T]: T[K] extends Function ? K : never;
+}[keyof T];
+
 export function bindMethods<T extends object>(instance: T) {
-  return <K extends keyof T>(...methods: K[]): Pick<T, K> => {
+  return <K extends FunctionKeys<T>>(...methods: [K, ...K[]]): Pick<T, K> => {
     return methods.reduce(
       (acc, key) => {
         const fn = instance[key];
