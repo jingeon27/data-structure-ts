3d7ee0356ade6223e06d0734dc33d9c42d3830cf
gun kim <gun@whattime.co.kr>
2025-04-13 23:34:24 +0900

    functionKeys로 타입 안정성 챙기기

--- react/src/common/bindMap.ts
+++ react/src/common/bindMap.ts
@@ -1,11 +1,14 @@
 export function bindMethods<T extends object>(instance: T) {
   return <K extends keyof T>(...methods: K[]): Pick<T, K> => {
-    return methods.reduce((acc, key) => {
-      const fn = instance[key];
-      if (typeof fn === 'function') {
-        acc[key] = fn.bind(instance);
-      }
-      return acc;
-    }, {} as any);
+    return methods.reduce(
+      (acc, key) => {
+        const fn = instance[key];
+        if (typeof fn === 'function') {
+          acc[key] = fn.bind(instance);
+        }
+        return acc;
+      },
+      {} as Pick<T, K>,
+    );
   };
 }
